933a6511230942fd5d8379b9f677753519211ba8
Antonio <antoarcejr@gmail.com>
2022-09-15 11:32:20 -0400

    Daily Class practice files

---
+++ Class Practice/090822.py
@@ -0,0 +1,42 @@
+#This is a comment in python
+'''
+Three singles quotes 
+for a multi line comment
+'''
+""" 
+double quotes also works
+"""
+num1 = 3
+num1
+print (num1)
+import math
+from turtle import clear
+print(4**0.5)
+print(math.sqrt(4))
+
+
+print(math.pow(2,3))
+
+print(math.pi)
+
+num=3.0
+print(type(num))
+num2=7.0
+print(int(num2))
+print(type(num2))
+num2=int(num2)
+print(type(num2))
+print()
+print("I\'m using tilt controls!") #use backslash(esc character) to have quotes work
+
+print()
+print('line1')
+print("line2")
+print('use \n to print to a new line')
+print('line3')
+
+message = 'hello'
+print(message[0])
+print(message[1])
+print(message[-1])
+print(message[len(message)-1])  #len is length method
---
+++ Class Practice/091322.py
@@ -0,0 +1,26 @@
+#September 18th
+
+my_list = [1,2,3]
+print(my_list)
+
+print
+
+my_list2 =  ['hello',25,67.22,'0']
+print(len(my_list2))
+print(my_list2)
+print(my_list+my_list2)
+print(my_list*2)
+print(5 in my_list)
+print(my_list[0])
+print(my_list[-1])
+
+print
+
+grades = [95,83,71]
+average = sum(grades)/len(grades)
+print(average)
+
+print
+
+my_tuple = (2,3.5,True,'Hello')
+print(my_tuple*4)
